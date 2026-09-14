1e8ac8acdb172eb7bf1ab44e10bddec992b02cee
Camille Allard <c.allard@cadulis.com>
2026-09-14 11:18:57 +0200

    test(footer): add unit test for github repository link

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -10,7 +10,7 @@ describe('FooterComponent', () => {
     await TestBed.configureTestingModule({
       imports: [FooterComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,16 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the secure GitHub link', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const link = compiled.querySelector<HTMLAnchorElement>('a.github-link');
+
+    expect(link).toBeTruthy();
+    expect(link?.getAttribute('href')).toBe('https://github.com/AllardCamille/Portfolio');
+    expect(link?.getAttribute('target')).toBe('_blank');
+    expect(link?.getAttribute('rel')).toContain('noopener');
+    expect(link?.getAttribute('rel')).toContain('noreferrer');
   });
 });
